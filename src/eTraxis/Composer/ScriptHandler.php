d782c67a20a025e4d80bbbd8859d041e4621ce1c
Artem Rodygin <artem.rodygin@gmail.com>
2015-06-11 10:44:22 +1200

    Custom scripts for Composer

---
+++ src/eTraxis/Composer/ScriptHandler.php
@@ -0,0 +1,69 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2015 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis.  If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+
+namespace eTraxis\Composer;
+
+/**
+ * Custom scripts for Composer.
+ */
+class ScriptHandler
+{
+    /**
+     * Installs required npm modules.
+     */
+    public static function installNpmModules()
+    {
+        $modules = [
+            'bower',
+            'gulp',
+            'gulp-add-src',
+            'gulp-concat',
+            'gulp-less',
+            'gulp-minify-css',
+            'gulp-rename',
+            'gulp-strip-json-comments',
+            'gulp-uglify',
+            'gulp-watch',
+            'fs',
+            'merge-stream',
+        ];
+
+        print("\nInstalling npm modules\n");
+        system('npm install ' . implode(' ', $modules));
+    }
+
+    /**
+     * Makes Bower to install front-end libraries, then executes Gulp to process them.
+     */
+    public static function installAssets()
+    {
+        print("\nInstalling assets\n");
+        system('bower install');
+
+        print("\nProcessing assets\n");
+        system('gulp');
+    }
+
+    /**
+     * Makes Bower to update front-end libraries, then executes Gulp to process them.
+     */
+    public static function updateAssets()
+    {
+        print("\nUpdating assets\n");
+        system('bower update');
+
+        print("\nProcessing assets\n");
+        system('gulp');
+    }
+}
