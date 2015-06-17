b1a51bccc9cb80073157349ebcdeec171d922c6a
Artem Rodygin <artem.rodygin@gmail.com>
2015-06-17 16:58:48 +1200

    DataTables: block UI on AJAX requests

--- app/Resources/public/js/table.js
+++ app/Resources/public/js/table.js
@@ -22,7 +22,7 @@ var datatables_language = window.datatables_language || {};
 
             jQueryUI: true,
             stateSave: true,
-            processing: true,
+            processing: false,
             serverSide: true,
             checkboxes: false,
 
@@ -40,14 +40,59 @@ var datatables_language = window.datatables_language || {};
 
         var settings = $.extend(defaults, options);
 
+        // Timer to block the table while AJAX request is being processed.
+        var blockTimer = null;
+
+        // ID of the last AJAX request.
+        // First AJAX request doesn't produce "preXhr.dr" event, but does produce the "xhr.dt" with "1" as draw number.
+        var drawNumber = 1;
+
+        // Block the table before AJAX request is sent.
+        function tableBlock($table) {
+
+            // Most requests are supposed to be processed within few milliseconds.
+            // To avoid visual flickering we initially block with invisible overlay.
+            $table.closest('.dataTables_wrapper').block({
+                message: null,
+                title: null,
+                theme: false,
+                overlayCSS: {
+                    opacity: 0
+                }
+            });
+
+            // If response is not received for a long, re-block with visible overlay.
+            blockTimer = setTimeout(function() {
+                $table.closest('.dataTables_wrapper').block({
+                    message: eTraxis.i18n.PleaseWait,
+                    title: null,
+                    theme: true,
+                    themedCSS: {
+                        padding: '10px'
+                    }
+                });
+            }, 500);
+        }
+
+        // Unblock the table when server response is received.
+        function tableUnblock($table) {
+            clearTimeout(blockTimer);
+            $table.closest('.dataTables_wrapper').unblock();
+        }
+
+        // Make table width autoadjustable.
         $(this).prop('width', '100%');
 
+        // In case of "checkboxes" feature...
         if (settings.checkboxes) {
 
-            settings.order = [1, 'asc'];
-
+            // ...prepend the header with one more column...
             $('thead tr', this).prepend('<th><input type="checkbox" name="characters"></th>');
 
+            // ... and disable ordering by this column.
+            settings.order = [1, 'asc'];
+
+            // Custom rendering of the first column to convert data into value of a checkbox.
             settings.columnDefs.push({
                 targets: 0,
                 orderable: false,
@@ -58,18 +103,41 @@ var datatables_language = window.datatables_language || {};
             });
         }
 
+        // Call DataTables plugin.
         var $table = $(this).dataTable(settings);
 
+        // In case of "checkboxes" feature...
         if (settings.checkboxes) {
 
+            // ... implement "check all"/"uncheck all" ability for the checkbox in the header.
             $table.on('click', 'thead input[type="checkbox"]', function() {
                 $('tbody input[type="checkbox"]', $table).prop('checked', $(this).prop('checked'));
             });
 
+            // Avoid "click" event on the first column.
             $table.on('click', 'tbody tr td:first-child', function(e) {
                 e.stopPropagation();
             });
         }
+
+        // Before each AJAX request block the table until server response.
+        $table.on('preXhr.dt', function(e, settings, data) {
+            drawNumber = data.draw;
+            tableBlock($table);
+        });
+
+        // When server responded unblock the table.
+        $table.on('xhr.dt', function(e, settings, json) {
+            // In case of race condition we can receive a response on previous request,
+            // while recent request is still under processing.
+            // Unblock only if no more responses are expected.
+            if (json.draw == drawNumber) {
+                tableUnblock($table);
+            }
+        });
+
+        // Block the table while first AJAX request is under progress.
+        tableBlock($table);
 
         return $table;
     };
