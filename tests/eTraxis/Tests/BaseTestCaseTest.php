754db0c0824ff04a9541d662d301a57c61fa6434
Artem Rodygin <artem.rodygin@gmail.com>
2015-07-21 11:53:17 +1200

    More unit tests

---
+++ tests/eTraxis/Tests/BaseTestCaseTest.php
@@ -0,0 +1,34 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2014-2015 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis.  If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\Tests;
+
+class BaseTestCaseTest extends BaseTestCase
+{
+    public function testFindUser()
+    {
+        $this->assertNull($this->findUser('unknown'));
+
+        $user = $this->findUser('artem');
+
+        $this->assertInstanceOf('eTraxis\Entity\User', $user);
+        $this->assertEquals('artem', $user->getUsername());
+    }
+
+    public function testLoginAs()
+    {
+        $this->assertFalse($this->loginAs('unknown'));
+        $this->assertTrue($this->loginAs('artem'));
+        $this->assertTrue($this->loginAs('einstein', true));
+    }
+}
