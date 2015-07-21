2b7788940662f582c01d805f7cfa6a950cf4c1e2
Artem Rodygin <artem.rodygin@gmail.com>
2015-07-21 18:42:37 +1200

    More unit tests

---
+++ tests/eTraxis/SimpleBus/Middleware/ValidationExceptionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2015 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis.  If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\SimpleBus\Middleware;
+
+class ValidationExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetMessages()
+    {
+        $errors = [
+            'error1' => 'Message #1',
+            'error2' => 'Message #2',
+            'error3' => 'Message #3',
+        ];
+
+        $exception = new ValidationException($errors);
+
+        $this->assertTrue(is_array($exception->getMessages()));
+        $this->assertEquals($errors, $exception->getMessages());
+    }
+}
