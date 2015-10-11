3bf2446b80f4662d605d97a43569c0af59de1291
Artem Rodygin <artem.rodygin@gmail.com>
2015-10-12 08:57:32 +1300

    Simplify copyright notice

--- app/Resources/public/js/disable.js
+++ app/Resources/public/js/disable.js
@@ -1,10 +1,8 @@
 /*!
  *  Copyright (C) 2015 Artem Rodygin
  *
- *  This file is part of eTraxis.
- *
  *  You should have received a copy of the GNU General Public License
- *  along with eTraxis.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with the file. If not, see <http://www.gnu.org/licenses/>.
  */
 
 (function($) {
