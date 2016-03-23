339732f0f17552123232da400c4295bf7f5ce68d
Artem Rodygin <artem.rodygin@gmail.com>
2016-03-23 13:20:48 +1300

    [Travis] Build #246 was broken

--- tests/eTraxis/SimpleBus/Fields/DeleteFieldCommandTest.php
+++ tests/eTraxis/SimpleBus/Fields/DeleteFieldCommandTest.php
@@ -44,9 +44,9 @@ class DeleteFieldCommandTest extends BaseTestCase
         /** @var Field $field3 */
         /** @var Field $field4 */
         $field1 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Crew']);
-        $field2 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Delivery to']);
-        $field3 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Delivery at']);
-        $field4 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Notes']);
+        $field2 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Delivery to', 'stateId' => $field1->getStateId()]);
+        $field3 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Delivery at', 'stateId' => $field1->getStateId()]);
+        $field4 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Notes',       'stateId' => $field1->getStateId()]);
 
         $this->assertEquals(1, $field1->getIndexNumber());
         $this->assertEquals(2, $field2->getIndexNumber());
@@ -57,9 +57,9 @@ class DeleteFieldCommandTest extends BaseTestCase
         $this->command_bus->handle($command);
 
         $field1 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Crew']);
-        $field2 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Delivery to']);
-        $field3 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Delivery at']);
-        $field4 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Notes']);
+        $field2 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Delivery to', 'stateId' => $field1->getStateId()]);
+        $field3 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Delivery at', 'stateId' => $field1->getStateId()]);
+        $field4 = $this->doctrine->getRepository(Field::class)->findOneBy(['name' => 'Notes',       'stateId' => $field1->getStateId()]);
 
         $this->assertEquals(1, $field1->getIndexNumber());
         $this->assertNull($field2);
